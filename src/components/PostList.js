4b4d7d5bddaa98f09ddd167007d1d3cd818e50cc
Kevin Reyes <reyeskevin9767@gmail.com>
2020-09-03 19:07:33 -0400

    create and link action creator

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
+import { connect } from 'react-deux';
+import { fetchPosts } from '../actions';
 
 class PostList extends Component {
+  componentDidMount() {
+    this.props.fetchPosts();
+  }
+
   render() {
     return <div>Post List</div>;
   }
 }
 
-export default PostList;
+export default connect(null, { fetchPosts })(PostList);
